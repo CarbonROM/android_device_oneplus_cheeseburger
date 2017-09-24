0fbe4f55f8a952e5fbc42651b49762209aa20e35
Marko Man <darkobas@gmail.com>
2017-09-24 15:41:36 -0400

    cheeseburger: enable camera shim

--- device.mk
+++ device.mk
@@ -163,8 +163,15 @@ PRODUCT_PACKAGES += \
     libbt-vendor
 
 # Camera
-# PRODUCT_PACKAGES += \
-    # Snap
+PRODUCT_PACKAGES += \
+    libcamera_shim \
+    SnapdragonCamera
+
+PRODUCT_PACKAGES += camera.device@3.2-impl
+PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
