56213096fc4f7ccd9503cd4ee7a8478878320e02
gtpitch <bmontagu@gmail.com>
2017-10-19 20:53:45 -0400

    cheeseburger: aosip-ify
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -21,11 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cheeseburger device
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
-# Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := carbon_cheeseburger
+PRODUCT_NAME := aosip_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -40,8 +39,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5 PRODUCT_NAME=OnePlus5
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=OnePlus/OnePlus5/OnePlus5:7.1.1/NMF26X/10061059:user/release-keys
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="gtpitch"
+    DEVICE_MAINTAINERS="DreamTeam" \
 
 TARGET_VENDOR := oneplus
