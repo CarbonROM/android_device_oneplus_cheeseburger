1873fc45483d5ff36773f31d3063a19fe82c12a3
gtpitch <bmontagu@gmail.com>
2017-11-08 20:28:28 -0500

    Revert "cheeseburger: enable camera shim"
    This reverts commit a76fd06305f0311bd203a1708721d32158442b0c.

--- device.mk
+++ device.mk
@@ -162,19 +162,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
-# Camera configuration file. Shared by passthrough/binderized camera HAL
-PRODUCT_PACKAGES += camera.device@3.2-impl
-#PRODUCT_PACKAGES += camera.device@1.0-impl
-PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
-# Enable binderized camera HAL
-#PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
-
 # Camera
-PRODUCT_PACKAGES += \
-    libcamera_shim \
-    libcamera_shim_display \
-    libcamera_shim_buffer \
-    libcamera_shim_native
+# PRODUCT_PACKAGES += \
+    # Snap
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
