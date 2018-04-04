ec6ee1f77aa6eb0200fa0bc0d0d1f04294925540
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    cheeseburger: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Open Source Project
+# Copyright (C) 2017-2019 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/carbon_cheeseburger.mk
+
+COMMON_LUNCH_CHOICES := \
+    carbon_cheeseburger-user \
+    carbon_cheeseburger-userdebug \
+    carbon_cheeseburger-eng
